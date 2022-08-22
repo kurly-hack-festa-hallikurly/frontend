19be76f24ac46507b9324ab902a9d8aab4ca4f8c
DevPeterLim <dev.peter.lim@gmail.com>
2022-08-22 10:19:27 +0900

    added screensize restriction

--- kurly_front/src/pages/Home.jsx
+++ kurly_front/src/pages/Home.jsx
@@ -1,3 +1,4 @@
+import { useEffect, useState } from "react";
 import styled from "styled-components"
 
 import GlobalBottomNavi from "../components/GlobalBottomNavi";
@@ -7,17 +8,31 @@ import NavBar from "../components/NavBar";
 import ProductList from "../components/ProductList";
 
 const Home = () => {
-    return (
-        <>
-            <StBox>
-                <NavBar></NavBar>
-                <GlobalTopNavi></GlobalTopNavi>
-                <Jumbotron></Jumbotron>
-                <ProductList></ProductList>
-                <GlobalBottomNavi></GlobalBottomNavi>
-            </StBox>
-        </>
-    )
+    const [width, setWidth] = useState(window.innerWidth);
+    useEffect(()=>{
+        const windowResize = () => {
+            setWidth(window.innerWidth)
+            console.log(width);
+        }
+        window.addEventListener(`resize`, windowResize);
+    }, [width])
+    if (width>390) {
+        return <>모바일 사이즈에 최적화 되어 있습니다. 가로 사이즈를 390픽셀로 줄여주세요</>
+    } else {
+        return (
+            <>
+                <StBox>
+                    <NavBar></NavBar>
+                    <GlobalTopNavi></GlobalTopNavi>
+                    {width}
+                    <Jumbotron></Jumbotron>
+                    <ProductList></ProductList>
+                    <GlobalBottomNavi></GlobalBottomNavi>
+                </StBox>
+            </>
+        )
+    }
+    
 }
 
 const StBox = styled.div`
